31383b40e83f197967401794fddbb44f0dd8bd58
Alexandre COSTA MOREIRA <alexcostamoreira.pro@gmail.com>
2022-03-02 14:12:45 +0100

    Get movies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MovieDetailsComponent, MovieListComponent } from 'src/components';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+
+import { MovieDetailsComponent, MovieListComponent } from 'src/components';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -13,7 +14,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/components/movie-list/movie-list.component.ts
+++ src/components/movie-list/movie-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { MovieDto } from 'src/dtos';
 
 @Component({
   selector: 'app-movie-list',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-list.component.css']
 })
 export class MovieListComponent implements OnInit {
+  // private movies: MovieDto;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get<MovieDto[]>('https://movie-api.benoithubert.me/movies').subscribe(response => {
+      console.log(response);
+    });
   }
 
 }
