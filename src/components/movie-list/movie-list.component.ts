7c9d0db9674af2139f074258048099b5cc4ee90e
Alexandre COSTA MOREIRA <alexcostamoreira.pro@gmail.com>
2022-03-02 14:30:24 +0100

    Convert movies properties to camel case format

--- src/components/movie-list/movie-list.component.ts
+++ src/components/movie-list/movie-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import * as camelcaseKeys from 'camelcase-keys';
 import { MovieDto } from 'src/dtos';
 
 @Component({
@@ -8,14 +9,13 @@ import { MovieDto } from 'src/dtos';
   styleUrls: ['./movie-list.component.css']
 })
 export class MovieListComponent implements OnInit {
-  // private movies: MovieDto;
+  movies: MovieDto[] = [];
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.http.get<MovieDto[]>('https://movie-api.benoithubert.me/movies').subscribe(response => {
-      console.log(response);
+      this.movies = camelcaseKeys(response, { deep: true });
     });
   }
-
 }
